1f2ce4f1028cf42213a589a8972c310d63bdd956
izadorasobral <izadoracsobral@gmail.com>
2025-05-07 14:13:13 -0300

    Adding project files to the repository

---
+++ data_processing.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+df = pd.read_csv("FOCUS-Sample-Data/FOCUS-1.0/focus_sample_10000.csv")  # Ajuste o caminho, se necessário
+print(df.head())  # Exibe as primeiras linhas do dataset
